3d7d493e2e8a7c9d6e707d85278d80e469bdd665
taylorandstubbs <taylor@taylorstubbs.com>
2017-03-15 13:50:50 -0500

    Adding match making to server

--- index.js
+++ index.js
@@ -4,15 +4,45 @@ const firebase = require("firebase")
 const database = initFirebase()
 
 function listenForSearchThread() {
-    //TODO
+    database.ref("users").on("child_changed", (dataSnapshot) => {
+        const isUserSearchingForThread = dataSnapshot.child("searchingForThread").val()
+        if (isUserSearchingForThread) {
+            findUserForThread(dataSnapshot.key)
+        }
+    })
 }
 
 function initFirebase() {
     firebase.initializeApp({
         databaseURL: "https://whodischat.firebaseio.com/"
-    });
+    })
 
     return firebase.database()
+}
+
+function findUserForThread(firstUserId) {
+    database.ref("users").on("child_added", (dataSnapshot) => {
+        const matchUserId = dataSnapshot.key;
+        const isUserSearchingForThread = dataSnapshot.child("searchingForThread").val();
+
+        if (matchUserId != firstUserId && isUserSearchingForThread) {
+            //stop listening on match
+            database.ref("users").off("child_added", this)
+
+            //create new messageThread
+            const threadId = database.ref("messageThreads").push().key
+
+            database.ref("users").child(firstUserId).update(
+                {"messageThread": threadId,
+                "searchingForThread": false}
+            )
+
+            database.ref("users").child(matchUserId).update(
+                {"messageThread": threadId,
+                "searchingForThread": false}
+            )
+        }
+    })
 }
 
 //bootstrap
